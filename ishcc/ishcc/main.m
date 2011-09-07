a84d6144503b4b7cabab62985fd34ad41c8ce994
Josh McDonald <josh@expantra.com>
2011-09-07 13:39:24 +1000

    Init project

---
+++ ishcc/ishcc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ishcc
+//
+//  Created by Josh McDonald on 7/09/11.
+//  Copyright 2011 Expantra Pty. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
